a4fddbff779d172e3e2c7963654f2ab03446d1e1
Ian Segers <iansegers@gmail.com>
2019-12-02 12:09:51 +0100

    Added headers

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ const POD_NAME = process.env.POD_NAME || 'No POD_NAME specified';
 app.get('/', (req, res) => {
     const now = new Date();
     console.log(`WHOAMI received request on route "/" at ${now.toISOString()}`);
-    res.json({
+    res.set('Content-Type', 'text/plain');
+    res.send(JSON.stringify({
         message: `Hello World!`,
         timestamp: now.getTime(),
         timeUtc: now.toISOString(),
-        podName: POD_NAME
-    });
-
-
+        podName: POD_NAME,
+        requestHeaders: req.headers
+    }));
 });
 
 app.listen(PORT, () => console.log(`WHOAMI listening on port ${PORT}`));
